c5ebefe18dd868936722174e68aaa5ebc22045e5
Eduardo Acha <eduardoacha@gmail.com>
2016-07-18 21:04:14 -0400

    Move option definitions to scan.go

--- lib/scan/scan.go
+++ lib/scan/scan.go
@@ -1,6 +1,34 @@
 package scan
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"sync"
+	"time"
+)
+
+type Data interface{}
+
+type Options struct {
+	// Basic Scan Setup
+	WaitGroup *sync.WaitGroup
+
+	InputFileName  string
+	OutputFileName string
+
+	InputChan  chan string
+	OutputChan chan string
+
+	Port              int
+	Module            string
+	Protocol          string
+	Threads           uint
+	ConnectionTimeout time.Duration
+	IOTimeout         time.Duration
+
+	// More options in the future
+}
+
+type scannable func(*Options, string) Data
 
 func Scan(options *Options, statistic *Statistic, fn scannable) {
 	defer options.WaitGroup.Done()
