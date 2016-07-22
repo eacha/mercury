b07ff6327a1e0d093a271716d59e9f3453f6eaad
Eduardo Acha <eduardoacha@gmail.com>
2016-07-21 23:33:38 -0400

    Extract options to alone file

---
+++ lib/scan/options.go
@@ -0,0 +1,26 @@
+package scan
+
+import (
+	"sync"
+	"time"
+)
+
+type Options struct {
+	// Basic Scan Setup
+	WaitGroup *sync.WaitGroup
+
+	InputFileName  string
+	OutputFileName string
+
+	InputChan  chan string
+	OutputChan chan string
+
+	Port              int
+	Module            string
+	Protocol          string
+	Threads           uint
+	ConnectionTimeout time.Duration
+	IOTimeout         time.Duration
+
+	// More options in the future
+}
--- lib/scan/scan.go
+++ lib/scan/scan.go
@@ -2,31 +2,9 @@ package scan
 
 import (
 	"encoding/json"
-	"sync"
-	"time"
 )
 
 type Data interface{}
-
-type Options struct {
-	// Basic Scan Setup
-	WaitGroup *sync.WaitGroup
-
-	InputFileName  string
-	OutputFileName string
-
-	InputChan  chan string
-	OutputChan chan string
-
-	Port              int
-	Module            string
-	Protocol          string
-	Threads           uint
-	ConnectionTimeout time.Duration
-	IOTimeout         time.Duration
-
-	// More options in the future
-}
 
 type scannable func(*Options, string) Data
 
